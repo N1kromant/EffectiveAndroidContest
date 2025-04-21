2d6af3bf3849568e9e610f541f9ad4d792b75101
N1kromant <nikromka@gmail.com>
2025-04-22 01:18:54 +0600

    Task 5 soloved

--- app/src/main/java/com/github/radch_enko/effectiveandroidcontest/StoneScissorsPaper.kt
+++ app/src/main/java/com/github/radch_enko/effectiveandroidcontest/StoneScissorsPaper.kt
@@ -1,19 +1,38 @@
 package com.github.radch_enko.effectiveandroidcontest
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import com.github.radch_enko.effectiveandroidcontest.core.steps.Action
 import com.github.radch_enko.effectiveandroidcontest.core.steps.GetNextAction
 import com.github.radch_enko.effectiveandroidcontest.core.steps.StoneScissorsPaper
+import kotlin.random.Random
 
 @Composable
 fun StoneScissorsPaperScreen(onSuccess: () -> Unit) {
+    var i = 0
+
     StoneScissorsPaper(
         getNextAction = object : GetNextAction {
             override fun invoke(): Action {
-                // TODO реализуй свою стратегию
-                return Action.SCISSORS
+
+                val result = when(i) {
+                    in 0..5 -> getRandomAction()
+                    6 -> Action.STONE
+                    7 -> Action.SCISSORS
+                    8 -> Action.STONE
+                    9 -> Action.PAPER
+                    else -> { i = -1; Action.SCISSORS }
+                }
+                i++
+
+                return result
             }
         },
         onSuccess = onSuccess,
     )
+}
+
+fun getRandomAction(): Action {
+    val actions = Action.entries.toTypedArray()
+    return actions[Random.nextInt(actions.size)]
 }
