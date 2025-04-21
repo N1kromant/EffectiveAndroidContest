a5a505a4173f46bc9e91911fa63ba8b7d028b1e7
N1kromant <nikromka@gmail.com>
2025-04-21 21:30:58 +0600

    Task 3 soloved

--- app/src/main/java/com/github/radch_enko/effectiveandroidcontest/LaggyListScreen.kt
+++ app/src/main/java/com/github/radch_enko/effectiveandroidcontest/LaggyListScreen.kt
@@ -1,15 +1,21 @@
 package com.github.radch_enko.effectiveandroidcontest
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.github.radch_enko.effectiveandroidcontest.core.steps.LaggyListWrapper
 import com.github.radch_enko.effectiveandroidcontest.core.steps.ListItem
 
 @Composable
 fun LaggyListScreen(onSuccess: () -> Unit) {
-    LaggyListWrapper(onSuccess = onSuccess) { list ->
+    LaggyListWrapper(onSuccess = onSuccess) { _list ->
+        val list = remember { _list }
+
         Column(modifier = Modifier) {
+
             list.forEach {
                 ListItem(modifier = Modifier, rainbow = it)
             }
